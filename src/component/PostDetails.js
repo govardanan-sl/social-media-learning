629cd061e5f4527262942c925489865385e68ad4
govardanan-sl <govardanan.sl@rently.com>
2021-04-22 13:10:47 +0530

    Updated Post details'

--- src/component/PostDetails.js
+++ src/component/PostDetails.js
@@ -1,10 +1,19 @@
 import {useParams} from 'react-router-dom';
+import useFetch from '../useFetch';
 const PostDetails = () =>{
     const { id } = useParams();
-
+    const {data:post ,error, isLoading} = useFetch("http://localhost:8000/posts/"+id);
     return (
         <div className="post-details">
-            <h2>Post Details {id}</h2>
+            {isLoading&&<div>Loading Post...</div>}
+            {error &&<div>{error}</div>}
+            {post &&(
+                <article>
+                    <h2>{post.title}</h2>
+                    <p>posted by {post.author}</p>
+                    <div>{post.body}</div>
+                </article>
+            )}
         </div>
     );
 }
